cbc98564fb4d7617d2efbb373779735e1071c423
A J <lov2code@gmail.com>
2020-11-17 21:29:59 +0530

    generate serial and create empty

--- Shipping-Project/shipping.py
+++ Shipping-Project/shipping.py
@@ -1,12 +1,19 @@
 
-class ShippingContainer:
-    next_serial = 22
 
+class ShippingContainer:
+
+    next_serial = 8334
+
+    # Moving out the serial number generation from init and created a static method
     @staticmethod
     def _generate_serial():
         result = ShippingContainer.next_serial
         ShippingContainer.next_serial += 1
         return result
+
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code,[])
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
