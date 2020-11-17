50311ff0386dd8fce8c3b2b1a0bb0e57d4b99b60
A J <lov2code@gmail.com>
2020-11-17 22:37:56 +0530

    new bic method and updated the init

--- Shipping-Project/shipping.py
+++ Shipping-Project/shipping.py
@@ -1,4 +1,5 @@
 
+import iso6346
 
 class ShippingContainer:
 
@@ -11,14 +12,24 @@ class ShippingContainer:
         ShippingContainer.next_serial += 1
         return result
 
-    # This helps to create an object with empty contents
+    # create bic code for shipping container using iso6436
+    @staticmethod
+    def _create_bic_code(owner_code, serial):
+        return iso6346.create(owner_code, str(serial).zfill(6))
+
+    # class method to create a shipping with empty contents
     @classmethod
     def create_empty(cls, owner_code):
         return cls(owner_code,[])
 
+    # class method to create a shipping with list of items
+    @classmethod
+    def create_list(cls, owner_code, list_items):
+        return cls(owner_code, contents=list(list_items))
+
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._create_bic_code(owner_code, ShippingContainer._generate_serial())
 
 
