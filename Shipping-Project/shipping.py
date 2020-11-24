563400df21ca170975d069943a86ccb2ef28684d
A J <lov2code@gmail.com>
2020-11-24 23:20:52 +0530

    heated refrigerator shipping container

--- Shipping-Project/shipping.py
+++ Shipping-Project/shipping.py
@@ -3,6 +3,9 @@ import iso6346
 
 class ShippingContainer:
 
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
     next_serial = 8334
 
     # Changing this to a class method
@@ -19,16 +22,24 @@ class ShippingContainer:
 
     # class method to create a shipping with empty contents
     @classmethod
-    def create_empty(cls, owner_code, **kwargs):
-        return cls(owner_code, contents=[], **kwargs)
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents=[], **kwargs)
 
     # class method to create a shipping with list of items
     @classmethod
-    def create_list(cls, owner_code, list_items, **kwargs):
-        return cls(owner_code, contents=list(list_items), **kwargs)
+    def create_list(cls, owner_code, length_ft, list_items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(list_items), **kwargs)
 
-    def __init__(self, owner_code, contents, **kwargs):
+    @property
+    def volume_ft(self):
+        return self._calc_volume()
+
+    def _calc_volume(self):
+        return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
+
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
         self.bic = self._create_bic_code(owner_code, ShippingContainer.generate_serial())
 
@@ -36,6 +47,7 @@ class ShippingContainer:
 class RefrigeratorShippingContainer(ShippingContainer):
 
     MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME = 100
 
     # Bic code generation for refrigerator shipping container
     @staticmethod
@@ -43,11 +55,14 @@ class RefrigeratorShippingContainer(ShippingContainer):
         return iso6346.create(owner_code, str(serial).zfill(6), category="R")
 
     # Adding temperature to the constructor
-    def __init__(self, owner_code, contents, *, celsius, **kwargs):
-        super().__init__(owner_code, contents, **kwargs)
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, length_ft, contents, **kwargs)
         # if celsius > RefrigeratorShippingContainer.MAX_CELSIUS:
         #     raise ValueError("Temperature too hot")
         self.celsius = celsius
+
+    def _calc_volume(self):
+        return (super()._calc_volume() - RefrigeratorShippingContainer.FRIDGE_VOLUME)
 
     # property for celsius
     @property
@@ -57,6 +72,9 @@ class RefrigeratorShippingContainer(ShippingContainer):
     # setter for celsius
     @celsius.setter
     def celsius(self, value):
+        return self._set_celsius(value)
+
+    def _set_celsius(self, value):
         if value > RefrigeratorShippingContainer.MAX_CELSIUS :
             raise ValueError("Temperature is too hot")
         self._celsius = value
@@ -80,4 +98,15 @@ class RefrigeratorShippingContainer(ShippingContainer):
     @fahrenheit.setter
     def fahrenheit(self, value):
         self.celsius = RefrigeratorShippingContainer._f_to_c(value)
+
+
+class HeatedRefrigeratorShippingContainer(RefrigeratorShippingContainer):
+    MIN_CELSIUS = -20
+
+
+    def _set_celsius(self, value):
+        if value < HeatedRefrigeratorShippingContainer.MIN_CELSIUS:
+            raise ValueError("Temperature too cold")
+        super()._set_celsius(value)
+
 
